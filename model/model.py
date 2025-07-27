14b5267c1738a69a589b0aa7fa6e1108fb2a2536
fabian.moenkeberg <fabian.moenkeberg@ziemergroup.com>
2025-07-27 18:16:46 +0200

    define model

---
+++ model/model.py
@@ -0,0 +1,317 @@
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+from dataclasses import dataclass
+import inspect
+
+
+@dataclass
+class GPTConfig:
+    block_size: int = 1024  # maximum sequence/context length
+    vocab_size: int = 50257  # number of tokens in the vocabulary: 50000 + 256 bytes + 1 endof-sequence token. Note the 256 byte tokens are used to ensure that each byte can be tokenized. (BPE is tokenizing on the byte level, thus remaining bytes could be handled here)
+    n_layer: int = 12  # number of layers in the transformer
+    n_head: int = 12  # number of attention heads
+    n_embd: int = 768  # embedding dimension
+
+
+class GPT(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.config = config
+
+        self.transformer = nn.ModuleDict(
+            dict(
+                wte=nn.Embedding(config.vocab_size, config.n_embd),
+                wpe=nn.Embedding(config.block_size, config.n_embd),
+                h=nn.ModuleList([Block(config) for _ in range(config.n_layer)]),
+                ln_f=nn.LayerNorm(config.n_embd),
+            )
+        )
+        self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
+
+        # weight sharing scheme
+        self.transformer.wte.weight = self.lm_head.weight
+
+        self.apply(self._init_weights)
+
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            std = 0.02
+            # in the original paper they scale the weights in the last projection
+            # by the number of layers (sqrt) to control the variance of the output
+            if hasattr(module, "dummy_flag"):
+                std *= (2 * self.config.n_layer) ** -0.5
+            torch.nn.init.normal_(module.weight, mean=0.0, std=std)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Embedding):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+
+    def configure_optimizers(self, weight_decay=0.1, learning_rate=3e-4, device="cpu"):
+        # start with all the candidate parameters
+        param_dict = {pn: p for pn, p in self.named_parameters()}
+        param_dict = {pn: p for pn, p in param_dict.items()}
+        # create optim groups. Any parameters that is 2D will be weight decayed,
+        # otherwise not
+        decay_params = [p for n, p in param_dict.items() if p.dim() >= 2]
+        nodecay_params = [p for n, p in param_dict.items() if p.dim() < 2]
+        optim_groups = [
+            {"params": decay_params, "weight_decay": weight_decay},
+            {"params": nodecay_params, "weight_decay": 0.0},
+        ]
+        num_decay_params = sum(p.numel() for p in decay_params)
+        num_nodecay_params = sum(p.numel() for p in nodecay_params)
+        print(f"[GPT] Number of parameters with weight decay: {num_decay_params:,}")
+        print(
+            f"[GPT] Number of parameters without weight decay: {num_nodecay_params:,}"
+        )
+        fused_available = "fused" in inspect.signature(torch.optim.AdamW).parameters
+        use_fused = fused_available and "cuda" in device
+        # this avoids iterating over the parameters if using GPU and if option is
+        # available
+        print(f"\tUsing fused AdamW: {use_fused}")
+        optimizer = torch.optim.AdamW(
+            optim_groups, lr=learning_rate, betas=(0.9, 0.95), eps=1e-8, fused=use_fused
+        )
+        return optimizer
+
+    def forward(self, idx, targets=None):
+        # idx is of shape (B, T)
+        B, T = idx.size()
+
+        assert (
+            T <= self.config.block_size
+        ), "Cannot forward sequence of length %d, block size is only %d" % (
+            T,
+            self.config.block_size,
+        )
+
+        # forward the token and position embeddings
+        pos = torch.arange(0, T, dtype=torch.long, device=idx.device)
+        pos_emb = self.transformer.wpe(pos)[None, :, :]  # (1, T, n_embd)
+        tok_emb = self.transformer.wte(idx)  # (B, T, n_embd)
+        x = tok_emb + pos_emb
+
+        for block in self.transformer.h:
+            x = block(x)
+
+        x = self.transformer.ln_f(x)  # (B, T, n_embd)
+        logits = self.lm_head(x)  # (B, T, vocab_size)
+        loss = None
+        if targets is not None:
+            loss = F.cross_entropy(
+                logits.view(-1, logits.size(-1)), targets.view(-1), ignore_index=-1
+            )
+        return logits, loss
+
+    def sample_sequence(
+        self,
+        prompt,
+        data_manger,
+        encoder,
+        num_return_sequences,
+        max_length,
+        top_priority=50,
+        stream=True,
+    ):
+        self.eval()
+
+        # encode prompt and reshape it into the desired shape
+        tokens = encoder.encode(prompt)
+        tokens = torch.tensor(tokens, dtype=torch.long)
+        tokens = tokens.unsqueeze(0).repeat(num_return_sequences, 1)
+
+        xgen = tokens.to(data_manger.device)
+
+        # create generator to sample from output
+        sample_rng = torch.Generator(device=data_manger.device)
+        sample_rng.manual_seed(42 + data_manger.ddp_rank)
+        if stream:
+            for seq_idx in range(num_return_sequences):
+                current_xgen = tokens[seq_idx : seq_idx + 1].to(
+                    data_manger.device
+                )  # Start with one sequence
+
+                # Decode and print initial prompt
+                initial_decoded_prompt = encoder.decode(current_xgen[0].tolist())
+                print(
+                    initial_decoded_prompt, end="", flush=True
+                )  # Print without newline and flush
+
+                while current_xgen.size(1) < max_length:
+                    with torch.no_grad():
+                        logits, _ = self.forward(current_xgen)
+                        # only keep the last prediction
+                        logits = logits[:, -1, :]
+                        probs = F.softmax(logits, dim=-1)
+                        topk_probs, topk_indices = torch.topk(
+                            probs, top_priority, dim=-1
+                        )
+                        ix = torch.multinomial(
+                            topk_probs, num_samples=1, generator=sample_rng
+                        )
+                        xcol = torch.gather(topk_indices, dim=-1, index=ix)
+                        current_xgen = torch.cat((current_xgen, xcol), dim=1)
+
+                        # Decode and print the newly generated token
+                        new_token = xcol[0].item()  # Get the single new token
+                        decoded_token = encoder.decode([new_token])  # Decode it
+                        print(decoded_token, end="", flush=True)  # Print and flush
+                print("\n")  # Add a newline after each sequence is complete.
+        else:
+            while xgen.size(1) < max_length:
+                with torch.no_grad():
+                    logits, _ = self.forward(xgen)
+                    # only keep the last prediction
+                    logits = logits[:, -1, :]
+                    probs = F.softmax(logits, dim=-1)
+                    topk_probs, topk_indices = torch.topk(probs, top_priority, dim=-1)
+                    ix = torch.multinomial(
+                        topk_probs, num_samples=1, generator=sample_rng
+                    )
+                    xcol = torch.gather(topk_indices, dim=-1, index=ix)
+                    xgen = torch.cat((xgen, xcol), dim=1)
+
+            for i in range(num_return_sequences):
+                tokens = xgen[i, :max_length].tolist()
+                # decode tokens back to vocabulary
+                decoded = encoder.decode(tokens)
+                print(f"rank {data_manger.ddp_rank} sample {i}: {decoded}")
+
+    @classmethod
+    def from_pretrained(cls, model_type):
+        """Loads pretrained GPT-2 model weights from huggingface"""
+        assert model_type in {"gpt2", "gpt2-medium", "gpt2-large", "gpt2-xl"}
+
+        from transformers import GPT2LMHeadModel
+
+        print("loading weights from pretrained gpt: %s" % model_type)
+
+        # n_layer, n_head and n_embd are determined from model_type
+        config_args = {
+            "gpt2": dict(n_layer=12, n_head=12, n_embd=768),
+            "gpt2-medium": dict(n_layer=24, n_head=16, n_embd=1024),
+            "gpt2-large": dict(n_layer=36, n_head=20, n_embd=1280),
+            "gpt2-xl": dict(n_layer=48, n_head=25, n_embd=1600),
+        }[model_type]
+        config_args["vocab_size"] = 50257
+        config_args["block_size"] = 1024
+        config = GPTConfig(**config_args)
+        model = GPT(config)
+        sd = model.state_dict()
+        sd_keys = sd.keys()
+        sd_keys = [k for k in sd_keys if not k.endswith(".attn.bias")]
+
+        # init a huggingface/transformers model
+        model_hf = GPT2LMHeadModel.from_pretrained(model_type)
+        sd_hf = model_hf.state_dict()
+
+        sd_keys_hf = sd_hf.keys()
+        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith(".attn.masked_bias")]
+        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith(".attn.bias")]
+        transposed = [
+            "attn.c_attn.weight",
+            "attn.c_proj.weight",
+            "mlp.c_fc.weight",
+            "mlp.c_proj.weight",
+        ]
+        assert len(sd_keys_hf) == len(
+            sd_keys
+        ), f"mismatched keys: {len(sd_keys_hf)} vs {len(sd_keys)}"
+
+        for k in sd_keys_hf:
+            if any(k.endswith(w) for w in transposed):
+                assert sd_hf[k].shape[::-1] == sd[k].shape
+                with torch.no_grad():
+                    sd[k].copy_(sd_hf[k].T)
+            else:
+                assert sd_hf[k].shape == sd[k].shape
+                with torch.no_grad():
+                    sd[k].copy_(sd_hf[k])
+
+        return model
+
+
+class CausalSelfAttention(nn.Module):
+    """Multi-head self-attention with a causal mask.
+    This is the communication layer of the transformer block.
+    It computes the attention scores and applies them to the values."""
+    def __init__(self, config):
+        super().__init__()
+        assert config.n_embd % config.n_head == 0
+        # key, query, value have n_embd // n_head features each
+        self.c_attn = nn.Linear(config.n_embd, config.n_embd * 3)
+        # output projection
+        self.c_proj = nn.Linear(config.n_embd, config.n_embd)
+
+        self.c_proj.dummy_flag = 1
+        self.n_head = config.n_head
+        self.n_embd = config.n_embd
+        self.register_buffer(
+            "bias",
+            torch.tril(torch.ones(config.block_size, config.block_size)).view(
+                1, 1, config.block_size, config.block_size
+            ),
+        )
+
+    def forward(self, x):
+        B, T, C = x.size()  # batch size, sequence length, embedding dimension
+        # calculate query, key, values for all heads in batch
+        # nh is "number of heads", hs is "head size", and C (number of channels)
+        # is nh * hs
+        # e.g. in GPT-2 with n_head=12 and nhs=64, we have C = nh*hs = 768
+        qkv = self.c_attn(x)
+        q, k, v = qkv.split(self.n_embd, dim=2)
+        # Note, we reshape the tensors to have the shape (B, T, nh, hs) such that we treat the vectors as a tensor with self.n_head*B batches (both are treated as batches).
+        # -> This results that the following operations are done in parallel for all heads.
+        k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)  # (B, nh, T, hs)
+        q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)  # (B, nh, T, hs)
+        v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)  # (B, nh, T, hs)
+        # this is the standard attention mechanism
+        # att = (q @ k.transpose(-2, -1)) * (1.0 / (C // self.n_head) ** 0.5)  # (B, nh, T, T)
+        # att = att.masked_fill(self.bias[:,:,:T,:T] == 0, float('-inf')) -> # this is the causal mask, it prevents attention to future tokens
+        # att = F.softmax(att, dim=-1)
+        # y = att @ v  # (B, nh, T, hs)
+        # this uses flash-attention: way faster (especially on GPU)
+        y = F.scaled_dot_product_attention(q, k, v, is_causal=True)
+
+        y = y.transpose(1, 2).contiguous().view(B, T, C)
+        y = self.c_proj(y)
+        return y
+
+
+class MLP(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.c_fc = nn.Linear(config.n_embd, 4 * config.n_embd)
+        self.gelu = nn.GELU(approximate="tanh")  # use tanh approximation for GELU, not the exact one. Note that with the new implementation of erf() needed in GELU, the code is as fast as with the approximated one. -> Use the exact one.
+        
+        self.c_proj = nn.Linear(4 * config.n_embd, config.n_embd)
+
+    def forward(self, x):
+        x = self.c_fc(x)
+        x = self.gelu(x)
+        x = self.c_proj(x)
+        return x
+
+
+class Block(nn.Module):
+    """A single Transformer block.
+    It consists of a self-attention layer and an MLP, each layer precedes a layer norm.
+    Note this is a difference to the original model from "Attention is All You Need" paper,
+    where the layer norm is applied after the self-attention and MLP layers. 
+    -> This is the "pre-LN" variant and more modern/stable.
+    Additionally, the residual connections are now not merged into the normalization.
+    -> This is a clean residual pathway and this is better for training stability and convergence.
+    """
+    def __init__(self, config):
+        super().__init__()
+        self.ln_1 = nn.LayerNorm(config.n_embd)
+        self.attn = CausalSelfAttention(config)  # This is the communication layer
+        self.ln_2 = nn.LayerNorm(config.n_embd)
+        self.mlp = MLP(config)  # This is the mapping layer, no communication here.
+
+    def forward(self, x):
+        x = x + self.attn(self.ln_1(x))
+        x = x + self.mlp(self.ln_2(x))
+        return x
